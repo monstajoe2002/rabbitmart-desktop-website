7947ed49bfede3d9eedfb04bf223fdcc0d87409f
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-15 20:23:00 +0200

    my first commit

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/my-cart"> Cart</a>
+                <a href="/account">Account</a>
+                <input type="text" placeholder="Search.."/>
+                
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
