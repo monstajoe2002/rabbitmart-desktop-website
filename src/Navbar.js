01d222163f3d8ba4faeed040f9dc42cb31b11909
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-16 17:54:40 +0200

    a few fixes here and there, no biggie 🤷‍♂️

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,10 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
+            <h1>Rabbit</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/my-cart"> Cart</a>
+                <a href="/my-cart">Cart</a>
                 <a href="/account">Account</a>
                 <input type="text" placeholder="Search.."/>
                 
