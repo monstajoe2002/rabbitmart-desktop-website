c0fa9e864c1492b0f2e053409ee0231a3422e337
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-18 10:40:58 +0200

    Added offers carousel as a seperate .jsx element

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import './App.css';
 import Navbar from './Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container, Carousel } from "react-bootstrap"
+import Offers from './components/Offers'
 import CategoryList from './components/CategoryList';
 function App() {
   return (
 
     <div className="App">
       <Navbar />
-      <Container>
-        <Carousel>
-          <Carousel.Item interval={1000}>
-            <img className="img-fluid" src='https://thumbs.dreamstime.com/b/three-bottles-lipton-ice-tea-soft-drink-poznan-pol-jun-plastic-brand-sold-belonging-to-unilever-british-dutch-150243220.jpg'/>
-          </Carousel.Item>
-          <Carousel.Item interval={1000}>
-            <img className="img-fluid" src='https://insiteooh.com/storage/app/uploads/public/5dd/669/e55/5dd669e55228c181021042.jpg' />
-          </Carousel.Item>
-        </Carousel>
-      </Container>
+      <Offers/>
       <h1 class="h1">Categories</h1>
       <Container>
         <CategoryList />
