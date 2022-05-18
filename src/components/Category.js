f2abfdae2a0bc74fd284f2e1e17a34b5ab84f001
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-18 15:22:30 +0200

    fixed everything tgif 🙏

--- src/components/Category.js
+++ src/components/Category.js
@@ -5,34 +5,19 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 const Category = () => {
     return ( 
-<<<<<<< HEAD
         <Card sx={{ maxWidth: 100, maxHeight:400 }}>
             <CardActionArea>
                 <CardMedia
                     component="img"
                     
-=======
-        <Card sx={{ maxWidth: 100, maxHeight: 400 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-
->>>>>>> origin/youssef-amr
                     image="https://cdnprod.mafretailproxy.com/sys-master-root/h60/h9e/10066911100958/362476_main.jpg_1700Wx1700H"
                     alt="Lamar Milk"
                 />
                 <CardContent>
-<<<<<<< HEAD
                     <Typography  style={{fontFamily:'Poppins'}} gutterBottom variant="h5" component="div">
                         Lamar Milk
                     </Typography>
                     
-=======
-                    <Typography style={{ fontFamily: 'Poppins' }} gutterBottom variant="h5" component="div">
-                        Lamar Milk
-                    </Typography>
-
->>>>>>> origin/youssef-amr
                 </CardContent>
             </CardActionArea>
         </Card>
--- src/components/Offers.js
+++ src/components/Offers.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/youssef-amr
 import { Container, Carousel } from "react-bootstrap"
 const Offers = () => {
     return ( <div>
         <Container>
-<<<<<<< HEAD
         <Carousel>
             <Carousel.Item interval={1000}>
                 <img className="img-fluid" src='https://thumbs.dreamstime.com/b/three-bottles-lipton-ice-tea-soft-drink-poznan-pol-jun-plastic-brand-sold-belonging-to-unilever-british-dutch-150243220.jpg' />
@@ -18,18 +13,6 @@ const Offers = () => {
     </Container>
     </div> 
     );
-=======
-            <Carousel>
-                <Carousel.Item interval={1000}>
-                    <img className="img-fluid" src='https://thumbs.dreamstime.com/b/three-bottles-lipton-ice-tea-soft-drink-poznan-pol-jun-plastic-brand-sold-belonging-to-unilever-british-dutch-150243220.jpg' />
-                </Carousel.Item>
-                <Carousel.Item interval={1000}>
-                    <img className="img-fluid" src='https://insiteooh.com/storage/app/uploads/public/5dd/669/e55/5dd669e55228c181021042.jpg' />
-                </Carousel.Item>
-            </Carousel>
-        </Container>
-    </div> );
->>>>>>> origin/youssef-amr
 }
  
 export default Offers;
