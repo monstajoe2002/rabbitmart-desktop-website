dcd9d6b71f2677c208b2ce2a17b5b807b0c64546
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-18 13:29:36 +0200

    Added a category Card

---
+++ src/components/Category.js
@@ -0,0 +1,27 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+const Category = () => {
+    return ( 
+        <Card sx={{ maxWidth: 100, maxHeight: 400 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+
+                    image="https://cdnprod.mafretailproxy.com/sys-master-root/h60/h9e/10066911100958/362476_main.jpg_1700Wx1700H"
+                    alt="Lamar Milk"
+                />
+                <CardContent>
+                    <Typography style={{ fontFamily: 'Poppins' }} gutterBottom variant="h5" component="div">
+                        Lamar Milk
+                    </Typography>
+
+                </CardContent>
+            </CardActionArea>
+        </Card>
+     );
+}
+ 
+export default Category;
