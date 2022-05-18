9a27360d7e04016b6225bf18c269965e875d4e31
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-18 11:10:06 +0200

    Using a new css framework called mui

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,16 +1,26 @@
-import { Card } from "react-bootstrap";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 const Category = () => {
     return ( 
-        <div className="d-flex flex-row flex-nowrap">
-            <Card>
-                <div className=" card card-body" style={{ display: 'inline-block' }}>
-                    <Card.Img variant="top" src="https://assets.epicurious.com/photos/57eebe2eb382c3c017d3fff0/16:9/w_2560%2Cc_limit/supermarket-shelves.jpg" />
-                    <Card.Body>
-                        <button className="category-button">All</button>
-                    </Card.Body>
-                </div>
-            </Card>
-        </div>
+        <Card sx={{ maxWidth: 100, maxHeight:400 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    
+                    image="https://cdnprod.mafretailproxy.com/sys-master-root/h60/h9e/10066911100958/362476_main.jpg_1700Wx1700H"
+                    alt="Lamar Milk"
+                />
+                <CardContent>
+                    <Typography  style={{fontFamily:'Poppins'}} gutterBottom variant="h5" component="div">
+                        Lamar Milk
+                    </Typography>
+                    
+                </CardContent>
+            </CardActionArea>
+        </Card>
      );
 }
  
