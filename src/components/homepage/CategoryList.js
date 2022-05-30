a5ab92629bbfd75765c5e5e4d02619e7df50f2dd
monstajoe2002 <youssef.amr90@gmail.com>
2022-05-30 20:44:33 +0200

    removed redundant home link and added routes

--- src/components/homepage/CategoryList.js
+++ src/components/homepage/CategoryList.js
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
 import { NavDropdown } from 'react-bootstrap';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import App from '../../App';
+import Category from '../categories/Category';
 const CategoryList = () => {
-    const categories=['All','Meat, Fish & Poultry', 'Dairy Products']
-    return (  
+    let category=''
+    const categories = ['All', 'Meat, Fish & Poultry', 'Dairy Products']
+    return (
         <div>
             <NavDropdown title="Categories" id="offcanvasNavbarDropdown-expand-sm">
                 {categories.map(category =>
-                    <NavDropdown.Item href={`/category/${category.toLowerCase()}`} style={{ fontWeight: '400', color: 'black' }}>{category}</NavDropdown.Item>
+                    <>
+                        <NavDropdown.Item href={`/category/${category.toLowerCase()}`} style={{ fontWeight: '400', color: 'black' }}>{category}</NavDropdown.Item>
+                        
+                        
+                    </>
                 )}
 
+                
+                
             </NavDropdown>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={`/category/${category.toLowerCase()}`} element={<Category />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
- 
+
 export default CategoryList;
