3c89784a41358728913a85f016594a79eeb65226
farouqahmed10 <100158731+farouqahmed10@users.noreply.github.com>
2022-06-01 05:56:53 +0200

    myfirstcommit

---
+++ src/Data.js
@@ -0,0 +1,33 @@
+let shopItemsData = [
+    {
+      id: "id1",
+      name: "Nestle Pure Life Water",
+      price: 5,
+      desc: "1.5 Liter Water",
+      img: "img-1.jpg",
+    },
+    {
+      id: "id2",
+      name: "Galaxy Smooth Milk Chocolate Bar",
+      price: 6,
+      desc: "90 gm, 12 x 4.5 x 4.5 centimeters",
+      img: "img-2.jpg",
+    },
+    {
+      id: "id3",
+      name: "Tilda Basmati Rice",
+      price: 130,
+      desc: "2kg",
+      img: "img-3.jpg",
+    },
+    {
+      id: "id4",
+      name: "Kellogg's Corn Flakes",
+      price: 140,
+      desc: "500g, Contains : B6 vitamins, Iron, Vitamin D, & protein",
+      img: "img-4.jpg",
+    },
+    
+  ];
+  
+  
