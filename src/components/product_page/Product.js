3b80911af0ead4fba6996e1b0531907da9fc4600
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-06-05 15:19:26 +0200

    added counter

--- src/components/product_page/Product.js
+++ src/components/product_page/Product.js
@@ -1,7 +1,15 @@
 import '../../styles/product_page.css'
-import Image from 'react-bootstrap/Image'
+import { useState, useEffect } from 'react'
 import { Button } from '@mui/material';
+
+
 const ProductPage = () => {
+    const [count, setCount] = useState(0)
+    const incrementCount = () => {
+        setCount(count + 1)
+       
+    }
+    { useEffect(() => { console.log(count) });} 
     return (<div className="product-info">
         <img id="image" src="https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png">
 
@@ -11,8 +19,11 @@ const ProductPage = () => {
             <h1 id="price" >50.00 EGP</h1>
         </div>
         <h5 id='quantity'>1 kg</h5>
-        <Button></Button>
-    </div> );
+        <Button variant="contained" onClick={() => incrementCount()} style={{ backgroundColor: "#124831", fontFamily: 'Poppins', color: "#dcfd51" }}>Add to Cart</Button>
+        <p>{count}</p>
+        
+
+    </div>);
 }
- 
+
 export default ProductPage;
