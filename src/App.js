0e42ca2746b4f3e316d9873943a14f2ee4f363b2
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-06-05 12:19:34 +0200

    created product info page and modified homepage

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/homepage/Home';
 import ShoppingCart from './components/shopping_cart/Cart';
+import ProductPage from './components/product_page/Product';
+import './styles/index.css'
 function App() {
   return (
 
     <Router>
       <div className="App">
         <Navbar />
-        
           <Switch>
             <Route exact path="/">
               <Home/>
             </Route>
           <Route exact path="/my-cart">
             <ShoppingCart />
+          </Route>
+          <Route exact path="/product/">
+            <ProductPage />
           </Route>
         </Switch>
         
