e1917183cbd39fedb8fd0d044ea248f902c5a1db
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-06-06 12:30:57 +0200

    added quantity button

--- src/components/product_page/Product.js
+++ src/components/product_page/Product.js
@@ -7,7 +7,10 @@ const ProductPage = () => {
     const [count, setCount] = useState(0)
     const incrementCount = () => {
         setCount(count + 1)
-       
+
+    }
+    const decrementCount = () => {
+        setCount(count - 1);
     }
     return (<div className="product-info">
         <img id="image" src="https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png">
@@ -16,9 +19,15 @@ const ProductPage = () => {
         <h1 id='name'>El Doha Rice</h1>
         <h5 id='quantity'>1 kg</h5>
         <h1 id="price" >50.00 EGP</h1>
-        <Button variant="contained" onClick={() => incrementCount()} style={{ backgroundColor: "#124831", fontFamily: 'Poppins', color: "#dcfd51" }}>Add to Cart</Button>
-        <h5>{count}</h5>
-        
+        <Button variant="contained" onClick={() => console.log('Added to cart!')} style={{ backgroundColor: "#124831", fontFamily: 'Poppins', color: "#dcfd51" }}>Add to Cart</Button>
+        <div className="quantity" style={{ textAlign: 'right' }}>
+
+            <Button variant="outlined" disabled={`true`?count===0:decrementCount()} onClick={() => decrementCount()} > - </Button>
+            <h3>{count}</h3>
+            <Button variant="outlined" onClick={() => incrementCount()}> + </Button>
+        </div>
+
+
 
     </div>);
 }
