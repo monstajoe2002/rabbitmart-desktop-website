cca8def201953d49330195132bfd6603bef93757
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-06-09 15:03:49 +0200

    Axios call products API

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/homepage/Home';
 import ShoppingCart from './components/shopping_cart/Cart';
+import { useEffect,useState } from "react";
 import ProductPage from './components/product_page/Product';
 import './styles/index.css'
 import { v4 } from 'uuid'
+const axios = require('axios')
 function App() {
-  return (
+  const [products, setProducts] = useState([]);
+  let productsList;
 
+  useEffect(() => {
+
+    (async () => {
+      const result = await axios.get('https://matrixbytes-products-microservice.vercel.app/api/products')
+      productsList = result.data
+      setProducts(productsList);
+    })();
+    console.log(products)
+  }, []);
+  return (
+    
     <Router>
       <div className="App">
         <Navbar />
