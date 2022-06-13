eb4417648b6ec6f60c6a6facade91bb72561feb1
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2022-06-13 13:01:57 +0200

    fixed cart table

--- src/components/shopping_cart/Cart.js
+++ src/components/shopping_cart/Cart.js
@@ -1,22 +1,14 @@
+import { Button } from "@mui/material";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Table } from "react-bootstrap";
 import { cart } from "../product_page/Product";
 const ShoppingCart = () => {
-    const LOCAL_STORAGE_KEY=localStorage.key(0)
-    const [cartProducts,setCartProducts]=useState([cart])
-    useEffect(() => {
-        const stored = JSON.parse(localStorage.getItem( LOCAL_STORAGE_KEY))
-        if (stored) setCartProducts(stored)
-    }, [])
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cartProducts))
-    }, [cartProducts])
-
+let totalPrice=0;
+cart.map(product => totalPrice+=product.price)
     
     return (<div>
         <Table responsive='xl'>
-            <tr>
             <thead>
                 <th>Name</th>
                 <th>Price</th>
@@ -36,10 +28,15 @@ const ShoppingCart = () => {
                         </td>
                         
                     </tr>
+                <tr>
+                    <td>Total price: {totalPrice} EGP</td>
+                    <td />
+                    <td />
+                </tr>
                 </tbody>
 
-            </tr>
         </Table>
+        <Button variant="contained"  style={{ backgroundColor: "#124831", fontFamily: 'Poppins', color: "#dcfd51" }}>Checkout</Button>
     </div>);
 }
 
